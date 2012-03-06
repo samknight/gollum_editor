a062f1f6700341c2134a18ef3ae3ab17531d5b77
Sam Knight <sam@samknight.co.uk>
2012-03-06 09:33:44 +0000

    Change version and remove initializer

--- lib/gollum_editor.rb
+++ lib/gollum_editor.rb
@@ -1,11 +1,3 @@
 require "gollum_editor/version"
 require "gollum_editor/form_builder"
 require "gollum_editor/form_helper"
-
-module Rails
-    class GollumEditor < Engine
-      initializer :assets do |config|
-        Rails.application.config.assets.precompile += %w( gollum_editor/gollum.js gollum_editor/gollum.css )
-      end
-    end
-end
